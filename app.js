2dbc5e1689d67ea38f0300df263e30fd4cec447a
Carlos Santos <carlos.santos@vileve.com.br>
2021-12-29 22:28:08 -0300

    finish project

--- app.js
+++ app.js
@@ -1,125 +1,77 @@
-const http = require("http");
-const express = require("express");
-const bodyParser = require("body-parser");
-const AzureStorageBlob = require("@azure/storage-blob");
-const { BlobServiceClient } = require("@azure/storage-blob");
-var cors = require("cors");
-const fileUpload = require("express-fileupload");
-const fs = require("fs");
-const {
-  FormRecognizerClient,
-  AzureKeyCredential,
-} = require("@azure/ai-form-recognizer");
+let express = require('express');
+let cors = require('cors');
+let unggah = require('express-fileupload');
+const fs = require('fs');
+const Tesseract = require('node-tesseract-ocr');
 
-const app = express();
+var app = express();
+app.use(cors());
+app.use(unggah());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/img', express.static('storage'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-const hostname = "http://localhost";
-
-const port = process.env.PORT || "3005";
-app.set("port", port);
-
-const server = http.createServer(app);
-server.listen(port);
-
-app.get('/', (req,res)=>{st
-  res.send('<h1>Node.js OCR - Azure Form Recognizer Receipt</h1>')
-})
-
-
-// enable files upload
-app.use(
-  fileUpload({
-    createParentPath: true,
-  })
-);
-
-app.use((req, res, next) => {
-  console.log("Enter CORS");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS, PUT"
-  );
-  next();
+app.get('/', (req, res) => {
+  res.send('<h1>Node.js OCR - Tysseract</h1>');
 });
 
-app.options("*", cors()); // include before other routes
+// const capturedImage = async (req, res, next) => {
+//   try {
+//     const path = './storage/ocr_image.jpeg'; // destination image path
+//     let imgdata = req.body.img; // get img as base64
+//     const base64Data = imgdata.replace(/^data:([A-Za-z-+/]+);base64,/, ''); // convert base64
+//     fs.writeFileSync(path, base64Data, { encoding: 'base64' }); // write img file
 
-app.use("/api/analyze", (req, res, next) => {
-  if (!req.files) {
-    return res.status(400).send("No files were uploaded.");
-  }
-  let file = req.files.file;
-  uploadPath = __dirname + "\\uploads\\" + new Date().getTime() + ".jpg";
-  file.mv(uploadPath, async () => {
-    recognizeForm(uploadPath).then((result) => {
-      return res.status(200).json({
-        output: result,
-      });
-    });
-  });
-});
+//     Tesseract.recognize('http://localhost:5000/img/ocr_image.jpeg', 'eng', {
+//       logger: (m) => console.log(m),
+//     }).then(({ data: { text } }) => {
+//       console.log(text);
+//       return res.send({
+//         image: imgdata,
+//         path: path,
+//         text: text,
+//       });
+//     });
+//   } catch (e) {
+//     next(e);
+//   }
+// };
+// app.post('/capture', capturedImage);
 
-async function recognizeForm(file) {
-  const endpoint = "https://handson-mba-fiap-form-ale.cognitiveservices.azure.com/";
-  const apiKey = "827bff0b7e224350a86ce0d5588b9355";
-  console.log("Entering Forms Recognizer");
-
-  let fileStream = fs.createReadStream(file);
-
-  const client = new FormRecognizerClient(
-    endpoint,
-    new AzureKeyCredential(apiKey)
-  );
-
-  const poller = await client.beginRecognizeReceipts(fileStream, {
-    contentType: "image/jpeg",
-    onProgress: (state) => { console.log(`status: ${state.status}`); }
-  });
-  
-  const [receipt] = await poller.pollUntilDone();
-
-  console.log("Reconheci recibo:");
-
-  console.log(receipt);
-
-  const forms = await poller.pollUntilDone();
- 
-  console.log(forms)
-
-  console.log("Forms:");
-  for (const form of forms || []) {
-    console.log(`${form.formType}, page range: ${form.pageRange}`);
-    console.log("Pages:");
-    for (const page of form.pages || []) {
-      console.log(`Page number: ${page.pageNumber}`);
-      console.log("Tables");
-      for (const table of page.tables || []) {
-        for (const cell of table.cells) {
-          console.log(
-            `cell (${cell.rowIndex},${cell.columnIndex}) ${cell.text}`
-          );
-        }
+app.post('/upload', (req, res) => {
+  if (req.files) {
+    console.log(req.files);
+    var unggahFile = req.files.file;
+    var namaFile = unggahFile.name;
+    unggahFile.mv('./storage/' + namaFile, (err) => {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      } else {
+        // console.log(namaFile)
+        // res.send(namaFile)
+        Tesseract.recognize(`./storage/${namaFile}`, 'eng', {
+          logger: (m) => console.log(m),
+        })
+          .then((read) => {
+            console.log(read);
+            res.send({
+              text: read,
+            });
+            // return res.send({
+            //   image: `http://localhost:5000/img/${namaFile}`,
+            //   path: `http://localhost:5000/img/${namaFile}`,
+            //   text: text,
+            // });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       }
-    }
-
-    console.log("Fields:");
-    for (const fieldName in form.fields) {
-      // each field is of type FormField
-      const field = form.fields[fieldName];
-      console.log(
-        `Field ${fieldName} has value '${field.value}' with a confidence score of ${field.confidence}`
-      );
-    }
+    });
   }
-  fs.unlinkSync(uploadPath);
-  return forms;
-}
+});
 
+app.listen(5000, () => {
+  console.log('Server aktif @port 5000!');
+});
